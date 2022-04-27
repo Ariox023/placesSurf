e005e3337ed8ecfbfdeada1d849785fbb66fd175
Andrei Mesilov <andreimesilov1993@mail.ru>
2022-04-27 17:39:02 +0300

    4.4

---
+++ lib/mocks.dart
@@ -0,0 +1,75 @@
+import 'package:places/domain/sight.dart';
+
+final List mocks = <Sight>[
+  Sight(
+    name: 'Кишинёв',
+    type: 'Sity',
+    details: 'Cтолица и крупнейший город Молдавии.',
+    lat: 47.0264,
+    lon: 28.8404,
+    url:
+        'https://upload.wikimedia.org/wikipedia/commons/6/6b/Vorota_kishineva.jpg',
+  ),
+  Sight(
+    name: 'Бельцы',
+    type: 'Sity',
+    details:
+        'Важнейший транспортный узел севера Молдавии, в состав которого входят две железнодорожные станции.',
+    lat: 47.4542,
+    lon: 27.5544,
+    url: 'http://www.prospect.md/images/gallery/belicy-moldova-8462-1.jpg',
+  ),
+  Sight(
+    name: 'Орхей',
+    type: 'Sity',
+    details: 'Город и муниципий в Молдавии, центр Оргеевского района.',
+    lat: 47.2402,
+    lon: 28.4828,
+    url: 'https://newsmaker.md/wp-content/uploads/2020/01/3-6-1024x681.jpg',
+  ),
+  Sight(
+    name: 'Старый Орхей',
+    type: 'Museum',
+    details:
+        'Историко-археологический комплекс, расположенный в 60 км к северо-востоку от Кишинёва, на реке Реут.',
+    lat: 47.3605,
+    lon: 28.5821,
+    url: 'https://fs.tonkosti.ru/0x/pt/0xptwn0803mokc40o8sgwck48.jpg',
+  ),
+  Sight(
+    name: 'Единцы',
+    type: 'Sity',
+    details: 'Город и муниципий в Молдавии, центр Единéцкого района.',
+    lat: 48.16806,
+    lon: 27.1818,
+    url: 'https://static.locals.md/2016/11/Edinet_1.jpg',
+  ),
+  Sight(
+    name: 'Рези́на',
+    type: 'Sity',
+    details: 'Город в Молдавии, центр Резинского района.',
+    lat: 47.66667,
+    lon: 28.605,
+    url:
+        'http://images2.moldovatrans.com/tur/imgbank/locations/20140424152313910.jpg',
+  ),
+  Sight(
+    name: 'Замок “Mimi”',
+    type: 'winery',
+    details:
+        'Замок Мими в настоящее время входит в число самых красивых архитектурных шедевров в мире вина.',
+    lat: 0.001,
+    lon: 0.001,
+    url:
+        'https://www.deltalight.com/frontend/files/projects/images/source/003002_REA01.jpg',
+  ),
+  Sight(
+    name: 'Монастырь Кэприяна',
+    type: 'Monastery',
+    details:
+        'В живописных Кодрах, в 40 км к северо-западу от Кишинева, расположен один из самых старинных монастырей Молдовы Каприяна. Дата основания монастыря 1429 год.',
+    lat: 47.116018,
+    lon: 28.5115,
+    url: 'http://aerotourmd.com/images/moldova/religious-tourism/capriana.jpg',
+  ),
+];
